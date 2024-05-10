c011923cf8b7151d4bace0d54c088c46ab1e5d39
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-05-09 21:14:11 -0300

    refactor: changed users table name

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -2,7 +2,7 @@ import pool from "../config/db";
 
 const getAll = async () => {
   try {
-    const users = await pool.query("SELECT * FROM user_table");
+    const users = await pool.query("SELECT * FROM users_table");
     return users.rows;
   } catch (error) {
     console.log(error);
@@ -12,7 +12,7 @@ const getAll = async () => {
 
 const getOne = async (id) => {
   try {
-    const users = await pool.query("SELECT * FROM user_table WHERE user_id = $1",[id] );
+    const users = await pool.query("SELECT * FROM users_table WHERE user_id = $1",[id] );
     return users.rows;
   } catch (error) {
     console.log(error);
@@ -22,11 +22,10 @@ const getOne = async (id) => {
 
 const addUser = async (user) => {
   const { type, email, phone, name, address, cnpj, authId } = user;
-  console.log(authId);
 
   try {
     const newUser = await pool.query(
-      "INSERT INTO user_table (tipo_usuario , email, telefone, nome_completo, endereco, cnpj, auth_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      "INSERT INTO users_table (tipo_usuario , email, telefone, nome_completo, endereco, cnpj, auth_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
       [type, email, phone, name, address, cnpj, authId]
     );
 
@@ -42,7 +41,7 @@ const updateUser = async (userId, user) => {
 
   try {
     const updatedUser = await pool.query(
-      "UPDATE user_table SET tipo_usuario = $1 , email = $2, telefone = $3, nome_completo = $4, endereco = $5, cnpj = $6 WHERE user_id = $7 RETURNING *",
+      "UPDATE users_table SET tipo_usuario = $1 , email = $2, telefone = $3, nome_completo = $4, endereco = $5, cnpj = $6 WHERE user_id = $7 RETURNING *",
       [type, email, phone, name, address, cnpj, userId]
     );
 
@@ -56,7 +55,7 @@ const updateUser = async (userId, user) => {
 const deleteUser = async (userId) => {
   try {
     const deletedUser = await pool.query(
-      "UPDATE user_table SET status_conta = 'inativo' WHERE user_id = $1 RETURNING *",
+      "UPDATE users_table SET status_conta = 'inativo' WHERE user_id = $1 RETURNING *",
       [userId]
     );
 
