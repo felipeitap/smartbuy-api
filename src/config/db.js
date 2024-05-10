07684c985985b0109e8e4c8908ea8d47ec785d89
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-05-09 21:15:48 -0300

    feat: creating data base tables during server start

--- src/config/db.js
+++ src/config/db.js
@@ -10,4 +10,48 @@ const pool = new Pool({
   port: 5432,
 });
 
+const createUserTableQuery = `
+CREATE TABLE IF NOT EXISTS users_table (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    tipo_usuario VARCHAR(255),
+    nome_completo VARCHAR(255),
+    email VARCHAR(255),
+    telefone VARCHAR(20),
+    endereco VARCHAR(255),
+    cnpj VARCHAR(20),
+    status_conta VARCHAR(10) DEFAULT 'ativo',
+    data_registro DATE DEFAULT CURRENT_DATE,
+    auth_id UUID REFERENCES auth_table(id)
+)
+`;
+
+const createAuthTableQuey = `
+CREATE TABLE IF NOT EXISTS auth_table (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  username VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL
+);
+`;
+
+async function createTables() {
+  try {
+    const client = await pool.connect();
+
+    await client.query(createAuthTableQuey);
+    await client.query(createUserTableQuery);
+
+    client.release();
+  } catch (error) {
+    console.error("Error during tables creation:", error);
+  }
+}
+
+createTables()
+  .then(() => {
+    console.log("Data base started sucessfuly.");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 export default pool;
