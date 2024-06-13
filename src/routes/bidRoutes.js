02df902ba89a779fd125c5b3f727c52ef184066f
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-06-12 23:37:22 -0300

    refactor: refactored bid routes

--- src/routes/bidRoutes.js
+++ src/routes/bidRoutes.js
@@ -5,13 +5,13 @@ import bidController from "../controllers/bid_controller";
 
 const router = express.Router();
 
-router.get("/bid/:id", bidController.getBid);
+router.get("/bid/:id", bidController.getBids);
 
-router.get("/bid", bidController.getBids);
+router.get("/bid", bidController.getBid);
 
 router.post("/bid", bidController.addBid);
 
-router.put("/bid/:id", bidController.rejectBid);
+router.put("/bid/:id", bidController.updateStatus);
 
 router.delete("/bid/:id", bidController.cancelBid);
 
