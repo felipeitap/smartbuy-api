176ae577b705b9339df7baa2468c4a8475bd4587
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-06-15 17:57:23 -0300

    fix: Adjustments to bd connection

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false, 
+  }
 });
 
 const createExtension = 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";';
