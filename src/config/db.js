cee11942647606f9eef97f64e07ed5a4c7bc022e
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-06-15 16:18:10 -0300

    feat: added db connection url into a .env

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: "localhost",
+  host:  process.env.DB_URL,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
