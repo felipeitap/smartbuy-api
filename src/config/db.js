1fe2aaa071f37a50786a09c9c37adfebcb19c7a6
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-06-15 16:55:08 -0300

    fix: ajusted db connection string

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host:  process.env.DB_URL,
+  host:  process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
