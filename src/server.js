a32989327c66e9140bc93eddba6b4e0e9fe02821
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-12-01 13:43:12 -0300

    fix: removido politicas de cors

--- src/server.js
+++ src/server.js
@@ -8,8 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://smart-buy-front.vercel.app",
-    methods: ["GET", "POST"]
+    origin: "*",
   },
 });
 
